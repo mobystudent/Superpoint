9be507a674185fb0a04ac8e9a9d173aca63adef7
mobystudent <mobystudent@gmail.com>
2021-02-21 22:18:11 +0300

    Add switch tabs and correct show steps

--- src/js/script.js
+++ src/js/script.js
@@ -7,6 +7,7 @@ $(window).on('load', () => {
 	showSettings();
 	toggleTabs();
 	showNav();
+	showSteps(1);
 });
 
 function showTabs() {
@@ -24,15 +25,41 @@ function showSettings() {
 
 function toggleTabs() {
 	$('.tabs__btn--point').click(function() {
+		if ($(this).parent().hasClass('tabs__item--active')) return;
+
 		const numberTab = $(this).data('tab');
+		const svgHref = $(this).find('.icon__item').attr('xlink:href');
 
 		$('.main__content').hide();
 		$(`.main__content[data-content=${numberTab}]`).show();
+
+		$('.tabs__item').map((i, item) => {
+			$(item).find('.icon__item').attr('xlink:href', `#tab${i + 1}`);
+			$(item).removeClass('tabs__item--active');
+		});
+
+		$(this).find('.icon__item').attr('xlink:href', `${svgHref}a`);
+		$(this).parent().addClass('tabs__item--active');
+
+		showSteps(numberTab);
 	});
 }
 
 function showNav() {
 	$('#burger').click(function() {
 		$(this).toggleClass('header__burger--active');
+	});
+}
+
+function showSteps(id) {
+	console.log(id);
+	$(`.main__content[data-content=${id}] .step__item`).map((i, item) => {
+		if ($(item).hasClass('step__item--done')) {
+			$(item).find('.icon__item').attr('xlink:href', `#step${i + 1}d`);
+		} else if ($(item).hasClass('step__item--active')) {
+			$(item).find('.icon__item').attr('xlink:href', `#step${i + 1}a`);
+		} else {
+			$(item).find('.icon__item').attr('xlink:href', `#step${i + 1}`);
+		}
 	});
 }
