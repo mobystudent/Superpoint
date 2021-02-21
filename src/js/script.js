6fa5a5c6382db8baae6b4bd93ab1e7e439f2defe
mobystudent <mobystudent@gmail.com>
2021-02-21 16:59:00 +0300

    Add tabs

--- src/js/script.js
+++ src/js/script.js
@@ -3,5 +3,12 @@
 import $ from 'jquery';
 
 $(window).on('load', () => {
-
+	showTabs();
 });
+
+function showTabs() {
+	$('#showtabs').click(function() {
+		$(this).toggleClass('tabs__btn--active');
+		$('.tabs__wrap').toggleClass('tabs__wrap--active');
+	});
+}
