e484bd410ab8bbe5cc696ac6aad891e1dc109c2f
mobystudent <mobystudent@gmail.com>
2021-02-21 18:30:29 +0300

    Add logic tabs, select, menu and add hover

--- src/js/script.js
+++ src/js/script.js
@@ -4,11 +4,28 @@ import $ from 'jquery';
 
 $(window).on('load', () => {
 	showTabs();
+	showSettings();
+	toggleTabs();
 });
 
 function showTabs() {
 	$('#showtabs').click(function() {
 		$(this).toggleClass('tabs__btn--active');
 		$('.tabs__wrap').toggleClass('tabs__wrap--active');
+	});
+}
+
+function showSettings() {
+	$('#showSettings').click(() => {
+		$('.header__list').slideToggle();
+	});
+}
+
+function toggleTabs() {
+	$('.tabs__btn--point').click(function() {
+		const numberTab = $(this).data('tab');
+
+		$('.main__content').hide();
+		$(`.main__content[data-content=${numberTab}]`).show();
 	});
 }
